bfc8b3c99a3f9e9fdfa45e15bb5e950ee5545b22
RAJ5242 <75838783+RAJ5242@users.noreply.github.com>
2021-08-02 16:20:56 +0530

    Add files via upload

---
+++ js/Code.js
@@ -0,0 +1,25 @@
+const accessCode1 = "VARIABLE";
+const accessCode2 = "FUNCTION";
+const accessCode3 = "DATABASE";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text("R E V B A I L A", 100,50)
+    fill("lightblue")
+    text("Hint: Always changing, not constant !!", 100,70)
+
+fill("white")
+    textSize(15)
+    text("CUTIFON", 700,150)
+    fill("lightblue")
+    text("Hint: Always changing, not constant !!", 700,170)
+    
+    fill("white")
+    textSize(15)
+    text("ATEDASBA", 100,250)
+    fill("lightblue")
+    text("Hint: Always changing, not constant !!", 100,270)
+    
+}
